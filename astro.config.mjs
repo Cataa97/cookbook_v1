adf8baf329ff22a6477abd3c48abbab0becfe550
Cata <catalina.castillo19c@gmail.com>
2023-07-06 10:49:49 -0400

    deploy config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  site: 'https://cookbook_v1.github.io',
+  base: 'cookbook_v1',
+})
